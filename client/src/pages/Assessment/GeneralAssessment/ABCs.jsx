b2c97dca09caa10bc730ea15aafef3ecb7af86e6
Colin <43153081+clemesu1@users.noreply.github.com>
2021-08-20 14:50:17 -0300

    Fixed minor issues with ABCs

--- client/src/pages/Assessment/GeneralAssessment/ABCs.jsx
+++ client/src/pages/Assessment/GeneralAssessment/ABCs.jsx
@@ -28,18 +28,20 @@ const ABCs = (props) => {
 				<Typography>Airway Status</Typography>
 			</Grid>
 
-			<Grid item xs={10} >
+			<Grid item xs={9} >
 				<Paper variant="outlined" className={classes.paper}>
 
 					<FormControl component="fieldset" fullWidth>
 						<RadioGroup row aria-label="airway-status" name="Airway_Status" value={state.Airway_Status} onChange={handleChange} >
-							<FormControlLabel value="clear" control={<Radio />} label="Clear" />
-							<FormControlLabel value="completely-obstructed" control={<Radio />} label="Completely Obstructed" />
-							<FormControlLabel value="partially-obstructed" control={<Radio />} label="Partially Obstructed" />
+							<FormControlLabel value="Clear" control={<Radio />} label="Clear" />
+							<FormControlLabel value="Completely Obstructed" control={<Radio />} label="Completely Obstructed" />
+							<FormControlLabel value="Partially Obstructed" control={<Radio />} label="Partially Obstructed" />
 						</RadioGroup>
 					</FormControl>
 				</Paper>
 			</Grid>
+			<Grid item xs={1} />
+
 			<Grid item xs={2}>
 				<Typography>Breathing</Typography>
 			</Grid>
@@ -49,8 +51,8 @@ const ABCs = (props) => {
 					<FormControl component="fieldset">
 						<FormLabel color="secondary" component="legend">Effort</FormLabel>
 						<RadioGroup aria-label="breath-effort" name="Breath_Effort" value={state.Breath_Effort} onChange={handleChange}>
-							<FormControlLabel value="effortless" control={<Radio />} label="Effortless" />
-							<FormControlLabel value="laboured" control={<Radio />} label="Laboured" />
+							<FormControlLabel value="Effortless" control={<Radio />} label="Effortless" />
+							<FormControlLabel value="Laboured" control={<Radio />} label="Laboured" />
 						</RadioGroup>
 					</FormControl>
 				</Paper>
@@ -61,22 +63,23 @@ const ABCs = (props) => {
 					<FormControl component="fieldset">
 						<FormLabel color="secondary" component="legend">Rate</FormLabel>
 						<RadioGroup aria-label="breath-rate" name="Breath_Rate" value={state.Breath_Rate} onChange={handleChange}>
-							<FormControlLabel value="fast" control={<Radio />} label="Fast" />
-							<FormControlLabel value="normal" control={<Radio />} label="Normal" />
-							<FormControlLabel value="slow" control={<Radio />} label="Slow" />
-							<FormControlLabel value="absent" control={<Radio />} label="Absent" />
+							<FormControlLabel value="Fast" control={<Radio />} label="Fast" />
+							<FormControlLabel value="Normal" control={<Radio />} label="Normal" />
+							<FormControlLabel value="Slow" control={<Radio />} label="Slow" />
+							<FormControlLabel value="Absent" control={<Radio />} label="Absent" />
 						</RadioGroup>
 					</FormControl>
 				</Paper>
 			</Grid>
+
 			<Grid item xs={3}>
 				<Paper variant="outlined" className={classes.paper}>
 
 					<FormControl component="fieldset" >
 						<FormLabel color="secondary" component="legend">Rhythm</FormLabel>
 						<RadioGroup aria-label="breath-rhythm" name="Breath_Rhythm" value={state.Breath_Rhythm} onChange={handleChange}>
-							<FormControlLabel value="regular" control={<Radio />} label="Regular" />
-							<FormControlLabel value="irregular" control={<Radio />} label="Irregular" />
+							<FormControlLabel value="Regular" control={<Radio />} label="Regular" />
+							<FormControlLabel value="Irregular" control={<Radio />} label="Irregular" />
 						</RadioGroup>
 					</FormControl>
 				</Paper>
@@ -93,11 +96,11 @@ const ABCs = (props) => {
 						<FormControl component="fieldset" fullWidth>
 							<FormLabel color="secondary" component="legend">Site</FormLabel>
 							<RadioGroup row aria-label="circul-site" name="Circul_Site" value={state.Circul_Site} onChange={handleChange} style={{ justifyContent: 'space-between' }} >
-								<FormControlLabel value="carotid" control={<Radio />} label="Carotid" />
-								<FormControlLabel value="radial" control={<Radio />} label="Radial" />
-								<FormControlLabel value="brachial" control={<Radio />} label="Brachial" />
-								<FormControlLabel value="femoral" control={<Radio />} label="Femoral" />
-								<FormControlLabel value="apical" control={<Radio />} label="Apical" />
+								<FormControlLabel value="Carotid" control={<Radio />} label="Carotid" />
+								<FormControlLabel value="Radial" control={<Radio />} label="Radial" />
+								<FormControlLabel value="Brachial" control={<Radio />} label="Brachial" />
+								<FormControlLabel value="Femoral" control={<Radio />} label="Femoral" />
+								<FormControlLabel value="Apical" control={<Radio />} label="Apical" />
 							</RadioGroup>
 						</FormControl>
 					</Paper>
@@ -108,10 +111,10 @@ const ABCs = (props) => {
 						<FormControl component="fieldset">
 							<FormLabel color="secondary" component="legend">Rate</FormLabel>
 							<RadioGroup aria-label="circul-rate" name="Circul_Rate" value={state.Circul_Rate} onChange={handleChange}>
-								<FormControlLabel value="fast" control={<Radio />} label="Fast" />
-								<FormControlLabel value="normal" control={<Radio />} label="Normal" />
-								<FormControlLabel value="slow" control={<Radio />} label="Slow" />
-								<FormControlLabel value="absent" control={<Radio />} label="Absent" />
+								<FormControlLabel value="Fast" control={<Radio />} label="Fast" />
+								<FormControlLabel value="Normal" control={<Radio />} label="Normal" />
+								<FormControlLabel value="Slow" control={<Radio />} label="Slow" />
+								<FormControlLabel value="Absent" control={<Radio />} label="Absent" />
 							</RadioGroup>
 						</FormControl>
 					</Paper>
@@ -122,9 +125,9 @@ const ABCs = (props) => {
 						<FormControl component="fieldset">
 							<FormLabel color="secondary" component="legend">Volume</FormLabel>
 							<RadioGroup aria-label="circul-vol" name="Circul_Vol" value={state.Circul_Vol} onChange={handleChange}>
-								<FormControlLabel value="weak" control={<Radio />} label="Weak" />
-								<FormControlLabel value="normal" control={<Radio />} label="Normal" />
-								<FormControlLabel value="bounding" control={<Radio />} label="Bounding" />
+								<FormControlLabel value="Weak" control={<Radio />} label="Weak" />
+								<FormControlLabel value="Normal" control={<Radio />} label="Normal" />
+								<FormControlLabel value="Bounding" control={<Radio />} label="Bounding" />
 							</RadioGroup>
 						</FormControl>
 					</Paper>
@@ -135,8 +138,8 @@ const ABCs = (props) => {
 						<FormControl component="fieldset">
 							<FormLabel color="secondary" component="legend">Rhythm</FormLabel>
 							<RadioGroup aria-label="circul-rhythm" name="Circul_Rhythm" value={state.Circul_Rhythm} onChange={handleChange}>
-								<FormControlLabel value="regular" control={<Radio />} label="Regular" />
-								<FormControlLabel value="irregular" control={<Radio />} label="Irregular" />
+								<FormControlLabel value="Regular" control={<Radio />} label="Regular" />
+								<FormControlLabel value="Irregular" control={<Radio />} label="Irregular" />
 							</RadioGroup>
 						</FormControl>
 					</Paper>
